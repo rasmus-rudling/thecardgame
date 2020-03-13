e1017e47d80650198f8a99f4047524b8f3ee407a
Rasmus Rudling <rasmusrudling@gmail.com>
2020-03-13 18:54:40 +0100

    Updates

---
+++ precache-manifest.33513ef7cdead97154c93909fb7b10d4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd93712e5e90d06548469ddbbe500b30",
+    "url": "/ny_firebase_chatt/index.html"
+  },
+  {
+    "revision": "1d388663847a8d8a4689",
+    "url": "/ny_firebase_chatt/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a5154f14bdfd072a8846",
+    "url": "/ny_firebase_chatt/static/css/main.35446e21.chunk.css"
+  },
+  {
+    "revision": "1d388663847a8d8a4689",
+    "url": "/ny_firebase_chatt/static/js/2.f131c6d6.chunk.js"
+  },
+  {
+    "revision": "55cbd54242e5f05163b329a995cd8626",
+    "url": "/ny_firebase_chatt/static/js/2.f131c6d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5154f14bdfd072a8846",
+    "url": "/ny_firebase_chatt/static/js/main.1d9ee545.chunk.js"
+  },
+  {
+    "revision": "357aa1318e0c1542c1e6",
+    "url": "/ny_firebase_chatt/static/js/runtime-main.c65c30c9.js"
+  },
+  {
+    "revision": "db0b65ea37281e840fc4171fccfe1f59",
+    "url": "/ny_firebase_chatt/static/media/blue_card.db0b65ea.png"
+  },
+  {
+    "revision": "ef84d90862c84b67939332c033d471f8",
+    "url": "/ny_firebase_chatt/static/media/headerTextCards.ef84d908.png"
+  },
+  {
+    "revision": "2abfa2da10d3076ecef3f13d8e3424c8",
+    "url": "/ny_firebase_chatt/static/media/red_card.2abfa2da.png"
+  }
+]);
