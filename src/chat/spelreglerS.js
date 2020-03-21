1386529000154c8519df56c11b57822a1838c600
Johanna Simfors <jsimfors@gmail.com>
2020-03-21 16:31:51 +0100

    Headerimage uppdaterad. CSS finslipad

--- src/chat/spelreglerS.js
+++ src/chat/spelreglerS.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-var reglerShort = <div>Välkomment till The Card Game. Ni har nu blivit indelade i två lag.
- Börja med att läsa igenom reglerna genom att klicka på knappen 'spelregler' längst ned i fönstret.</div>;
+var reglerShort = <div>Välkommen till The Card Game. Ni har nu blivit indelade i två lag.
+ Börja med att läsa igenom reglerna genom att klicka på 'spelregler' längst ned i fönstret.</div>;
 
 export default reglerShort;
